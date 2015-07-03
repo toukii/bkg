5d31daf1255f3d4bf52c76867e77042de47fb501
shaalx <wisdomswing@gmail.com>
2015-07-03 15:23:24 +0800

    cmd

--- dir.go
+++ dir.go
@@ -1,26 +1,28 @@
 package bkg
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
-	"os"
-	"io/ioutil"
-	"fmt"
 )
 
-func ListDir(dir ,suf string) []string{
-	dirs,err:=ioutil.ReadDir(suf+dir)
-	if nil!=err {
+func ListDir(dir, suf string) []string {
+	dirs, err := ioutil.ReadDir(suf + dir)
+	if nil != err {
 		return nil
 	}
 	var ret []string
-	for _,it:=range dirs{
+	for _, it := range dirs {
 		if it.IsDir() {
-			sub_dirs:=ListDir(it.Name(),suf+"/")
-			ret=append(ret,sub_dirs...)
+			sub_dirs := ListDir(it.Name(), suf+"/")
+			ret = append(ret, sub_dirs...)
 			fmt.Println(sub_dirs)
-		}else{
+		} else {
 			fmt.Println(it.Name())
+			ret = append(ret, it.Name())
 		}
 		fmt.Println(it.Name())
 	}
@@ -29,25 +31,36 @@ func ListDir(dir ,suf string) []string{
 
 //获取指定目录及所有子目录下的所有文件，可以匹配后缀过滤。
 func WalkDir(dirPth, suffix string) (dirs []string, err error) {
- dirs = make([]string, 0, 30)
- suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+	dirs = make([]string, 0, 30)
+	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
 
- err = filepath.Walk(dirPth, func(filename string, fi os.FileInfo, err error) error { //遍历目录
-  //if err != nil { //忽略错误
-  // return err
-  //}
+	err = filepath.Walk(dirPth, func(filename string, fi os.FileInfo, err error) error { //遍历目录
+		//if err != nil { //忽略错误
+		// return err
+		//}
 
-  if !fi.IsDir() {
-   return nil
-  }
-  fmt.Println(fi.Name())
+		if !fi.IsDir() {
+			return nil
+		}
+		fmt.Println(fi.Name())
+		if strings.EqualFold(fi.Name(), ".git") {
+			return nil
+		}
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) {
+			dirs = append(dirs, filename)
+		}
 
-  if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) {
-   dirs = append(dirs, filename)
-  }
+		return nil
+	})
 
-  return nil
- })
+	return dirs, err
+}
 
- return dirs, err
+func CMD() {
+	output, err := exec.Command("go", "install").Output()
+	// err := cmd.Run()
+	if nil != err {
+		fmt.Println(err)
+	}
+	fmt.Println(string(output))
 }
--- dir_test.go
+++ dir_test.go
@@ -4,12 +4,17 @@ import (
 	"testing"
 )
 
-func TestListDir(t *testing.T) {
-	ret:= ListDir("$GOPATH/","")
-	t.Log(ret)
-}
+// func TestListDir(t *testing.T) {
+// 	var ret []string
+// 	ListDir("./", "/", &ret)
+// 	t.Log(ret)
+// }
 
-func TestWalkDir(t *testing.T) {
-	ret,_:= WalkDir("../",".go")
-	t.Log(ret)
+// func TestWalkDir(t *testing.T) {
+// 	ret,_:= WalkDir("../",".go")
+// 	t.Log(ret)
+// }
+
+func TestCMD(t *testing.T) {
+	CMD()
 }
