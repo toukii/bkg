20a5dbda2c0bda3a40008fbf5065cb0b8839a18e
shaalx <wisdomswing@gmail.com>
2015-07-15 16:18:17 +0800

    bkg

---
+++ bkg/dir_test.go
@@ -0,0 +1,21 @@
+package bkg
+
+import (
+	"testing"
+)
+
+// func TestListDir(t *testing.T) {
+// 	var ret []string
+// 	ListDir("./", "/", &ret)
+// 	t.Log(ret)
+// }
+
+// func TestWalkDir(t *testing.T) {
+// 	ret,_:= WalkDir("../",".go")
+// 	t.Log(ret)
+// }
+
+func TestCMD(t *testing.T) {
+	// CMD("E:/ItemForGo/src/github.com/shaalx/echo/oauth2")
+	CMD2()
+}
--- cmd.go
+++ cmd.go
@@ -1,93 +1,95 @@
-package bkg
+package main
 
-// package main
+import (
+	// "flag"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
 
-// import (
-// 	// "flag"
-// 	"log"
-// 	"os"
-// 	"os/exec"
-// 	"path/filepath"
-// 	"strings"
-// )
+var (
+	// command = flag.String("go", " ", "install")
+	cmd     = "go install"
+	command = &cmd
+)
 
-// var (
-// 	// command = flag.String("go", "", "install")
-// 	command = &("")
-// )
+func init() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+}
 
-// func init() {
-// 	log.SetFlags(log.Lshortfile | log.LstdFlags)
-// }
+func main() {
+	// flag.Parse()
+	if len(*command) == 0 {
+		log.Println("The argument '--command' is NOT specified!")
+		return
+	}
+	basePath, err := os.Getwd()
+	if err != nil {
+		log.Println("GetwdError:", err)
+		return
+	}
+	log.Printf("Base Path: %s", basePath)
+	baseDir, err := os.Open(basePath)
+	if err != nil {
+		log.Printf("OpenError (%s): %s", baseDir, err)
+		return
+	}
+	subDirs, err := baseDir.Readdir(-1)
+	if err != nil {
+		log.Println("ReaddirError:", err)
+		return
+	}
+	targetPaths := []string{}
+	for _, v := range subDirs {
+		fileInfo := v.(os.FileInfo)
+		if fileInfo.IsDir() {
+			if strings.EqualFold(fileInfo.Name(), ".git") {
+				continue
+			}
+			log.Printf("Target: %s", fileInfo.Name())
+			targetPaths = append(targetPaths, fileInfo.Name())
+		} else {
+			log.Printf("Non-target: %s", fileInfo.Name())
+		}
+	}
+	for _, v := range targetPaths {
+		err = os.Chdir(basePath)
+		if err != nil {
+			log.Printf("ChdirError (%s): %s", baseDir, err)
+			return
+		}
+		targetPath, err := filepath.Abs(v)
+		if err != nil {
+			log.Printf("AbsError (%s): %s", v, err)
+			return
+		}
+		log.Printf("Target Path: %s", targetPath)
+		err = os.Chdir(targetPath)
+		if err != nil {
+			log.Printf("ChdirError (%s): %s", targetPath, err)
+			return
+		}
+		cmdWithArgs := strings.Split(*command, " ")
+		var cmd *exec.Cmd
+		cmdLength := len(cmdWithArgs)
+		realCmd := cmdWithArgs[0]
+		var args []string
+		if cmdLength > 1 {
+			args = cmdWithArgs[1:cmdLength]
+			cmd = exec.Command(realCmd, args...)
+		} else {
+			cmd = exec.Command(realCmd)
+		}
+		result, err := cmd.Output()
+		if err != nil {
+			log.Printf("CmdRunError (cmd=%s, agrs=%v): %s", realCmd, args, err)
+			continue
+		}
+		log.Printf("Output (dir=%s, cmd=%s, agrs=%v): %v", targetPath, realCmd, args, string(result))
+	}
+	log.Println("The command(s) execution has been finished.")
+}
 
-// func main() {
-// 	// flag.Parse()
-// 	if len(*command) == 0 {
-// 		log.Println("The argument '--command' is NOT specified!")
-// 		return
-// 	}
-// 	basePath, err := os.Getwd()
-// 	if err != nil {
-// 		log.Println("GetwdError:", err)
-// 		return
-// 	}
-// 	log.Printf("Base Path: %s\\n", basePath)
-// 	baseDir, err := os.Open(basePath)
-// 	if err != nil {
-// 		log.Printf("OpenError (%s): %s\\n", baseDir, err)
-// 		return
-// 	}
-// 	subDirs, err := baseDir.Readdir(-1)
-// 	if err != nil {
-// 		log.Println("ReaddirError:", err)
-// 		return
-// 	}
-// 	targetPaths := []string{}
-// 	for _, v := range subDirs {
-// 		fileInfo := v.(os.FileInfo)
-// 		if fileInfo.IsDir() {
-// 			log.Printf("Target: %s\\n", fileInfo.Name())
-// 			targetPaths = append(targetPaths, fileInfo.Name())
-// 		} else {
-// 			log.Printf("Non-target: %s\\n", fileInfo.Name())
-// 		}
-// 	}
-// 	for _, v := range targetPaths {
-// 		err = os.Chdir(basePath)
-// 		if err != nil {
-// 			log.Printf("ChdirError (%s): %s\\n", baseDir, err)
-// 			return
-// 		}
-// 		targetPath, err := filepath.Abs(v)
-// 		if err != nil {
-// 			log.Printf("AbsError (%s): %s\\n", v, err)
-// 			return
-// 		}
-// 		log.Printf("Target Path: %s\\n", targetPath)
-// 		err = os.Chdir(targetPath)
-// 		if err != nil {
-// 			log.Printf("ChdirError (%s): %s\\n", targetPath, err)
-// 			return
-// 		}
-// 		cmdWithArgs := strings.Split(*command, " ")
-// 		var cmd *exec.Cmd
-// 		cmdLength := len(cmdWithArgs)
-// 		realCmd := cmdWithArgs[0]
-// 		var args []string
-// 		if cmdLength > 1 {
-// 			args = cmdWithArgs[1:cmdLength]
-// 			cmd = exec.Command(realCmd, args...)
-// 		} else {
-// 			cmd = exec.Command(realCmd)
-// 		}
-// 		result, err := cmd.Output()
-// 		if err != nil {
-// 			log.Printf("CmdRunError (cmd=%s, agrs=%v): %s\\n", realCmd, args, err)
-// 			return
-// 		}
-// 		log.Printf("Output (dir=%s, cmd=%s, agrs=%v): \\n%v\\n", targetPath, realCmd, args, string(result))
-// 	}
-// 	log.Println("The command(s) execution has been finished.")
-// }
-
-// //该片段来自于http://outofmemory.cn
+//该片段来自于http://outofmemory.cn
